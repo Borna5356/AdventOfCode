56e557586ca9bd0f12949d8d39b3a477e590c31f
Borna5356 <bornakevin1@gmail.com>
2021-12-04 13:03:20 -0500

    refactored part1

--- Day03/part1.py
+++ Day03/part1.py
@@ -1,17 +1,21 @@
-with open("data.txt", 'r') as file:
-    created_list = False
-    data = []
-    for line in file:
-        values = line.strip('\n')
-        if (created_list == False):
+def parse_file(filename):
+    with open(filename, 'r') as file:
+        created_list = False
+        data = []
+        for line in file:
+            values = line.strip('\n')
+            if (created_list == False):
+                for index in range(len(values)):
+                    data.append({'1': 0, '0': 0})
+                created_list = True
             for index in range(len(values)):
-                data.append({'1': 0, '0': 0})
-            created_list = True
-        for index in range(len(values)):
-            x = data[index]
-            value = values[index]
-            x[value] += 1
-            data[index] = x
+                x = data[index]
+                value = values[index]
+                x[value] += 1
+                data[index] = x
+        return data
+    
+def create_gamma_and_consumption(data):
     first_string = ""
     second_string = ""
     for element in data:
@@ -23,17 +27,24 @@ with open("data.txt", 'r') as file:
         else:
             first_string += '0'
             second_string += '1'
-    print(first_string)
-    print(second_string)
-    firstDecimal = 0
-    secondDecimal = 0
-    length = len(first_string)
+    return first_string, second_string
+
+def binary_to_decimal(binary):
+    result = 0
+    length = len(binary)
     for index in range(length):
-        value = first_string[index]
-        valueTwo = second_string[index]
+        value = binary[index]
         place = length - index - 1
-        firstDecimal += int(value) * 2**place 
-        secondDecimal += int(valueTwo) * 2**place
-    print(firstDecimal)
-    print(secondDecimal)
-    print(firstDecimal * secondDecimal)
+        result += int(value) * 2**place 
+    return result
+    
+ 
+def main():
+    data = parse_file("data.txt")
+    gamma, consumption = create_gamma_and_consumption(data)
+    valueOne = binary_to_decimal(gamma)
+    valueTwo = binary_to_decimal(consumption)
+    print(valueOne * valueTwo)
+
+if (__name__ == "__main__"):
+    main()
